0479ab18aaca28903ba2201396dae6a0cda7820c
Leo <lshapirov2@gmail.com>
2024-09-13 21:57:20 -0500

    index file, json files, connection and server files created

---
+++ index.js
@@ -0,0 +1,58 @@
+import inquirer from 'inquirer';
+
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit',
+      ]
+    }
+  ])
+  .then((answers) => {
+    switch (answer.action) {
+      case 'View all departments':
+        viewAllDepartments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      case 'Exit':
+        connection.end();
+        break;
+        default: 
+        console.log('Invalid action');
+        break;
+    }
+  }).catch((error) => {
+    console.error('Error:', error);
+  });
+
+  const viewAllDepartments = () => {
+
+  }
